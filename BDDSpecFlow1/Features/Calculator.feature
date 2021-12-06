ac06945330ce4fe6b98482cf02c97dd20009f1e4
paulmfinn <paulmfinn@gmail.com>
2021-12-06 10:52:59 +0000

    Add BBD Tests

---
+++ BDDSpecFlow1/Features/Calculator.feature
@@ -0,0 +1,26 @@
+﻿Feature: Calculator
+
+Scenario: Low Blood Pressure
+	Given the Systolic number is 89
+	And the Diastolic number is 50
+	When the BPCategory is calculated
+	Then the result should be Low
+
+Scenario: Ideal Blood Pressure
+	Given the Systolic number is 90
+	And the Diastolic number is 50
+	When the BPCategory is calculated
+	Then the result should be Ideal
+
+
+Scenario: PreHigh Blood Pressure
+	Given the Systolic number is 130
+	And the Diastolic number is 80
+	When the BPCategory is calculated
+	Then the result should be PreHigh
+
+Scenario: High Blood Pressure
+	Given the Systolic number is 170
+	And the Diastolic number is 80
+	When the BPCategory is calculated
+	Then the result should be High
---
+++ BDDSpecFlow1/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,46 @@
+﻿using TechTalk.SpecFlow;
+using BPCalculator;
+using FluentAssertions;
+
+namespace BDDSpecFlow1.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private readonly BloodPressure _bloodPressure = new BloodPressure();
+        private BPCategory _result;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"the Systolic number is (.*)")]
+        public void GivenTheSystolicNumberIs(int systolic)
+        {
+            _bloodPressure.Systolic = systolic;
+        }
+
+        [Given(@"the Diastolic number is (.*)")]
+        public void GivenTheDiastolicNumberIs(int diastolic)
+        {
+            _bloodPressure.Diastolic = diastolic;
+        }
+
+        [When(@"the BPCategory is calculated")]
+        public void WhenTheBPCategoryIsCalculated()
+        {
+            _result = _bloodPressure.Category;
+        }
+
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(BPCategory result)
+        {
+            //TODO: implement assert (verification) logic
+
+            _result.Should().Be(result);
+        }
+    }
+}
